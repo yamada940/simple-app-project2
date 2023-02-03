8056984e0b3e25738a8a1d0d6d3110c46f27625a
Yamada Yusaku <ootakimura.yusaku@gmail.com>
2023-02-04 00:16:42 +0900

    スライダーを作成しました

--- vite-project/assets/scripts/main.js
+++ vite-project/assets/scripts/main.js
@@ -1,8 +1,16 @@
-import '../styles/main.scss'
+import '../styles/main.scss';
+import '@splidejs/splide/css';
+import '@splidejs/splide/css/core';
+import '@splidejs/splide/css/skyblue';
+import Splide from '@splidejs/splide';
 import ActiveNav from './modules/activeNav';
 
 if(document.getElementById('js-nav') !== null) {
     new ActiveNav({
         navElement: 'js-nav'
     });
+}
+
+if(document.getElementsByClassName('splide')[0]){
+	new Splide( '.splide' ).mount();
 }
