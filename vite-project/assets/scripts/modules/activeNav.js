2eec482d685ab38c21a3fb1d2c63ebc35cebf7e2
Yamada Yusaku <ootakimura.yusaku@gmail.com>
2023-02-03 23:48:32 +0900

    ヘッダー部分の表示中ページに下線がつくようにしました

--- vite-project/assets/scripts/modules/activeNav.js
+++ vite-project/assets/scripts/modules/activeNav.js
@@ -1,25 +1,25 @@
 export default class ActiveNav {
-    constructor(obj) {
-        this.$nav = document.getElementById(obj.navElement);
-        this.ActiveNavStyle();
-    }
+	constructor(obj){
+		this.$nav = document.getElementById(obj.navElement);
+		this.activateNavStyle()
+	}
 
-    ActiveNavStyle() {
-        const $navItems = this.$nav.getElementsByTagName('a');
-        const url = window.location.href;
+	activateNavStyle() {
+		const $navItems = this.$nav.getElementsByTagName('a');
+		const url = window.location.href;
 
-        let topPageFlag = true;
+		let topPageFlag = true;
 
-        for(let index = 0; index < $navItems.length; index++) {
-            const pageKey = $navItems[index].dataset.page;
-            if(url.indexOf(pageKey) !== -1) {
-                $navItems[index].className += ' is-active';
-                topPageFlag = false;
-            }
-        }
+		for (let index = 0; index < $navItems.length; index++) {
+			const pageKey = $navItems[index].dataset.page;
+			if(url.indexOf(pageKey) !== -1){
+				$navItems[index].className += ' is-active';
+				topPageFlag = false;
+			}
+		}
 
-        if(topPageFlag) {
-            $navItems[0].className += ' is-active';
-        }
-    }
+		if(topPageFlag){
+			$navItems[0].className += ' is-active';
+		}
+	}
 }
