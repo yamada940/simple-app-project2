2c9a4c407ee9c85b999204a93538d283d1adc882
Yamada Yusaku <ootakimura.yusaku@gmail.com>
2023-02-03 23:36:56 +0900

    ActiveNavクラスを作成しました

---
+++ vite-project/assets/scripts/modules/activeNav.js
@@ -0,0 +1,25 @@
+export default class ActiveNav {
+    constructor(obj) {
+        this.$nav = document.getElementById(obj.navElement);
+        this.ActiveNavStyle();
+    }
+
+    ActiveNavStyle() {
+        const $navItems = this.$nav.getElementsByTagName('a');
+        const url = window.location.href;
+
+        let topPageFlag = true;
+
+        for(let index = 0; index < $navItems.length; index++) {
+            const pageKey = $navItems[index].dataset.page;
+            if(url.indexOf(pageKey) !== -1) {
+                $navItems[index].className += ' is-active';
+                topPageFlag = false;
+            }
+        }
+
+        if(topPageFlag) {
+            $navItems[0].className += ' is-active';
+        }
+    }
+}
