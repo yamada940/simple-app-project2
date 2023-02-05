2e6554c55d6180780107773c9d3d4401faf28d2b
Yamada Yusaku <ootakimura.yusaku@gmail.com>
2023-02-05 21:56:29 +0900

    .envファイルを作成しました

--- vite-project/assets/scripts/modules/const.js
+++ vite-project/assets/scripts/modules/const.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyCXtFudPDYqqVHrvfaWptPdCHE6_6J2XiA",
+    apiKey: import.meta.env.VITE_API_KEY,
     authDomain: "vite-project-3b6e4.firebaseapp.com",
-    projectId: "vite-project-3b6e4",
+    projectId: import.meta.env.VITE_APP_PROJECT_ID,
     storageBucket: "vite-project-3b6e4.appspot.com",
     messagingSenderId: "946444324653",
-    appId: "1:946444324653:web:6edb31c20f75906f5c0da4",
+    appId: import.meta.env.VITE_APP_ID,
     measurementId: "G-84W71KRGFW"
 };
